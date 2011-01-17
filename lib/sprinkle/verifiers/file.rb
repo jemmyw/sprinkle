0940972299d9e4194374a34e1e720d1cde6a1eef
Jeremy <jemmyw@gmail.com>
2011-01-17 14:15:05 +1300

    Add MD5 checking to has_file verifier

--- lib/sprinkle/verifiers/file.rb
+++ lib/sprinkle/verifiers/file.rb
@@ -14,8 +14,23 @@ module Sprinkle
       Sprinkle::Verify.register(Sprinkle::Verifiers::File)
       
       # Checks to make sure <tt>path</tt> is a file on the remote server.
-      def has_file(path)
-        @commands << "test -f #{path}"
+      def has_file(path, md5 = nil)
+        if md5
+          if md5 =~ /\/|\./
+            require 'md5'
+            newmd5 = MD5.new
+            ::File.open(md5, 'rb') do |input|
+              while bytes = input.read(4096)
+                newmd5.update(bytes)
+              end
+            end
+            md5 = newmd5.hexdigest
+          end
+
+          @commands << "echo \"#{md5}  #{path}\" | md5sum -c"
+        else
+          @commands << "test -f #{path}"
+        end
       end
       
       def file_contains(path, text)
